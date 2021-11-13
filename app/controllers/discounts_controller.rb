7384fa9416a2636bf298f5c2b667e2b04040a999
Devin Pile <devinpile@gmail.com>
2021-11-12 21:18:01 -0700

    Add new discount page and implement link from index

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,11 +1,15 @@
 class DiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @discounts = Discount.all
+    @discounts = @merchant.discounts
   end
 
   def show
     @merchant = Merchant.find(params[:merchant_id])
     @discount = Discount.find(params[:id])
+  end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
   end
 end
