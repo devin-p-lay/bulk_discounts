f88eb8a7df9103ae28af2c7cec539e0fbd5c3b80
Devin Pile <devinpile@gmail.com>
2021-11-13 22:15:31 -0700

    Add changes to config/application.rb for heroku

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
+config.assets.initialize_on_precompile = false
 require 'rails/all'
-# config.assets.initialize_on_precompile = false
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
